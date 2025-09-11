2206c466b68901b08dc095e733890ea401229b6a
StevenM711 <66960928+StevenM711@users.noreply.github.com>
2025-09-11 13:13:00 +0200

    Ensure log directory exists

--- cinbehave_gui.py
+++ cinbehave_gui.py
@@ -42,6 +42,7 @@ except ImportError:
     SLEAP_VERSION = "No instalado"
 
 # Configurar logging
+Path("logs").mkdir(exist_ok=True)
 logging.basicConfig(
     level=logging.INFO,
     format='%(asctime)s - %(levelname)s - %(message)s',
